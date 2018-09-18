bfeabbf9f253be95777f6b1e4d5bed689b29048a
JamieGainer <jgainer137@gmail.com>
2018-09-18 13:52:53 -0700

    Removed __init__.py and related files

--- build/example.py
+++ build/example.py
@@ -1,12 +1,21 @@
-from tpot import TPOTRegressor
+import autarchy
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 
 housing = load_boston()
+train_size = autarchy.config_dict['train-test']['train_size']
+test_size = autarchy.config_dict['train-test']['test_size']
+
 X_train, X_test, y_train, y_test = train_test_split(housing.data, housing.target,
                                                     train_size=0.75, test_size=0.25)
 
-tpot = TPOTRegressor(generations=5, population_size=20, verbosity=2)
+generations, population_size, verbosity = (autarchy.config_dict['genetic'][key]
+                                           for key in ['generations', 'population_size', 
+                                                       'verbosity'])
+tpot = autarchy.tpot.TPOTRegressor(generations=generations, 
+                                   population_size=population_size, 
+                                   verbosity=verbosity)
+
 tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
 tpot.export('tpot_boston_pipeline.py')
