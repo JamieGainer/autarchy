eb175027fc52c91a7d48bda59d64fa847c108c2f
JamieGainer <jgainer137@gmail.com>
2018-09-23 20:34:24 -0700

    Whitespace now all spaces.

--- meta_model_building/get_opt_params.py
+++ meta_model_building/get_opt_params.py
@@ -6,26 +6,26 @@ from sklearn.model_selection import train_test_split
 import time
 
 run_param = {
-	'random_state_train': 42,
-	'random_state_tpot': 42,
-	'export_python_code_filename': 'tpot_boston_pipeline.py',
-	'pickle_file_name': 'boston_pipeline_evaluated_models.pickle',
-	'train_size': 0.75,
-	'test_size': 0.25,
-	'generations': 5,
-	'population_size': 20,
-	'verbosity': 2,
-	'time': time.time()
+    'random_state_train': 42,
+    'random_state_tpot': 42,
+    'export_python_code_filename': 'tpot_boston_pipeline.py',
+    'pickle_file_name': 'boston_pipeline_evaluated_models.pickle',
+    'train_size': 0.75,
+    'test_size': 0.25,
+    'generations': 5,
+    'population_size': 20,
+    'verbosity': 2,
+    'time': time.time()
 }
 
 housing = load_boston()
 X_train, X_test, y_train, y_test = train_test_split(housing.data, housing.target,
                                                     train_size=run_param['train_size'], 
                                                     test_size=run_param['test_size'],
-						    random_state = run_param['random_state_train'])
+                                                    random_state = run_param['random_state_train'])
 
-tpot = TPOTRegressor(generations=run_param['generations'], population_size=run_param['population_size'], 
-					 verbosity=run_param['verbosity'], random_state = run_param['random_state_tpot'])
+tpot = TPOTRegressor(generations=run_param['generations'], population_size=run_param['population_size'],
+                    verbosity=run_param['verbosity'], random_state = run_param['random_state_tpot'])
 
 tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
@@ -37,4 +37,4 @@ pickle_dict.update(run_param)
 
 
 with open(run_param['pickle_file_name'], 'w') as pickle_file:
-	pickle.dump(pickle_dict, pickle_file)
+    pickle.dump(pickle_dict, pickle_file)
