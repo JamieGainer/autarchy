2abed629dc1871d4afc61fccb7ed25986d06b6d7
JamieGainer <jgainer137@gmail.com>
2018-09-25 06:59:06 -0700

    Docstring and header.

---
+++ meta_model_building/utility.py
@@ -0,0 +1,6 @@
+""" This module contains methods called in the main script for obtaining data:
+get_opt_params.py"""
+
+import copy
+from tpot.config.regressor import regressor_config_dict
+from tpot.config.regressor_light import regressor_config_dict_light
