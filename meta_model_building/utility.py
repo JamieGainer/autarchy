e43bf87f28596c073188bc451a1f71e60d3a8095
JamieGainer <jgainer137@gmail.com>
2018-09-25 07:01:30 -0700

    Added lists and dicts for choosing the model.

--- meta_model_building/utility.py
+++ meta_model_building/utility.py
@@ -4,3 +4,22 @@ get_opt_params.py"""
 import copy
 from tpot.config.regressor import regressor_config_dict
 from tpot.config.regressor_light import regressor_config_dict_light
+
+# model lists and dicts
+implemented_model_list = [
+    'regression', 'regression_light', 'knn',
+    'decision_tree', 'linear', 'SVR'
+    ]
+linear_model_list = [
+    'sklearn.linear_model.ElasticNetCV',
+    'sklearn.linear_model.LassoLarsCV',
+    'sklearn.linear_model.RidgeCV'
+    ]
+model_abbreviation_dict = {
+    'knn': 'sklearn.neighbors.KNeighborsRegressor',
+    'decision_tree': 'sklearn.tree.DecisionTreeRegressor',
+    'SVR': 'sklearn.svm.LinearSVR'
+}
+
+#preprocessor lists and dicts (to come)
+
