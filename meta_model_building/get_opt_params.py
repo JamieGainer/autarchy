aed26156da8f8118f2964c1747132e733e503e25
JamieGainer <jgainer137@gmail.com>
2018-09-24 17:09:09 -0700

    Set model choice at command line

--- meta_model_building/get_opt_params.py
+++ meta_model_building/get_opt_params.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 import sys
 import time
 
+model_list = ['regression', 'regression_light', 'knn', 'random_forest']
+
 if len(sys.argv) > 1:
     input_file_name = sys.argv[1]
     if sys.argv[1] == 'boston':
@@ -28,10 +30,11 @@ else:
     housing = load_boston()
     data, target = housing.data, housing.target
 
-if 'seed' in sys.argv:
+if '-seed' in sys.argv:
+    seed_position = sys.argv.index('-seed')
     try:
-        seed_position = sys.argv.index('seed')
         seed_value = int(sys.argv[seed_position + 1])
+        print('Random seeds set to', seed_value)
     except:
         seed_value = 42
         print('Random seeds set to 42')
@@ -39,24 +42,39 @@ else:
     seed_value = 42
     print('Random seeds set to 42')
 
-
+if '-model_space' in sys.argv:
+    ms_position = sys.argv.index('-model_space')
+    try:
+        model_space = sys.argv[ms_position + 1]
+        if model_space not in model_list:
+            print('Could not read model_space.  Aborting')
+            quit()
+    except:
+        print('Could not read model_space.  Aborting')
+        quit()
+else:
+    model_space = 'regression'
+print('Model space set to', model_space)
 
 seed = {
     'split_seed': seed_value,
-    'tpot_seed': seed_value}
+    'tpot_seed': seed_value
+    }
 
 meta_generations = 2
 
 split_param = {
     'train_size': 0.75,
-    'test_size': 0.25}
+    'test_size': 0.25
+    }
 
 run_param = {
     'population_size': 10,
     'verbosity': 1,
     'generations': 1,
     'random_state': seed['tpot_seed'],
-    'warm_start': True}
+    'warm_start': True
+    }
 
 housing = load_boston()
 x_train, x_test, y_train, y_test = train_test_split(
