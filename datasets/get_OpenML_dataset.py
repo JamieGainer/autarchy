d14659fb2d1e44b6a2866b7d2acb713a47943f20
JamieGainer <jgainer137@gmail.com>
2018-10-07 13:22:43 -0700

    Got downloads working

--- datasets/get_OpenML_dataset.py
+++ datasets/get_OpenML_dataset.py
@@ -21,26 +21,53 @@
 
     """
 
-def download_from_URL(url):
-    pass
+def download_from_URL(url, file_name):
+    import requests
+
+    with open(file_name, "wb") as csv_file:
+        print('Downloading', file_name, '...')
+        response = requests.get(url)
+        csv_file.write(response.content)
 
 
 def download_dataset(n, dataset_dict):
     import os
+
     dataset_entry = dataset_dict[n]
     file_name = dataset_entry['file_name']
+    url = dataset_entry['url']
+
     if file_name in os.listdir('.'):
         print(file_name, 'already downloaded.')
     else:
-        download_from_URL(dataset_dict[n]['url'])
+        download_from_URL(url, file_name)
+
 
 import sys
 import yaml
 
-print(len(sys.argv))
+try:
+    command = sys.argv[1]
+except IndexError:
+    print('Please specify dataset number or "all" to download all datasets.')
 
 with open('dataset_info.yaml', 'r') as yaml_file:
     dataset_dict = yaml.load(yaml_file)
+
+if command.upper() == 'ALL':
+    for key in dataset_dict:
+        download_dataset(key, dataset_dict)
+else:
+    try:
+        command = int(command)
+    except ValueError:
+        pass
+    if command in dataset_dict:
+        download_dataset(command, dataset_dict)
+    else:
+        print(command, 'not found')
+
+
 
 
 
