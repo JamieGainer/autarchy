874e49665743c4dce96b33e25cf9a239d1859aef
JamieGainer <jgainer137@gmail.com>
2018-10-07 08:49:08 -0700

    Start module to automate OpenML dataset downloads

---
+++ data/get_OpenML_dataset.py
@@ -0,0 +1,16 @@
+""" I have been testing AutoML on 22 datasets from OpenML that are
+    a subset of those listed in Balaji and Allen (2018)
+    https://arxiv.org/pdf/1808.06492.pdf.
+    These datasets require no preprocessing, e.g., to remove unsused string
+    information.
+
+    This module allows the user to download the dataset from OpenML
+    by the number I have (somewhat arbitrarily given it), or to 
+    download all such datasets.
+
+    Usage:
+
+
+    """
+
+
