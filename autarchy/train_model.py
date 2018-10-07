c9a8f702231d7e6d8d74eafad9ab6f8fde5457f2
JamieGainer <jgainer137@gmail.com>
2018-10-07 14:35:24 -0700

    Added csv file read-in

--- autarchy/train_model.py
+++ autarchy/train_model.py
@@ -2,7 +2,24 @@
 
     """
 
+import numpy as np
+import sys
 import tensorflow as tf
 from tensorflow import keras
 
+try:
+    input_file_name = sys.argv[1]
+except:
+    print('Need csv file name as first input.')
+    quit()
+
+try:
+    file_data = np.genfromtxt(input_file_name, delimiter=',', skip_header=1)
+except:
+    print('Unable to read data file:', input_file_name)
+    quit()
+
+data_shape = file_data.shape
+
+print(data_shape)
 
