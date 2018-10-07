3703f5313c9435a1356bedfa6647a8bdae96edb9
JamieGainer <jgainer137@gmail.com>
2018-10-06 17:58:36 -0700

    Add small setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autarchy",
+    version="0.0.1",
+    author="Jamie Gainer",
+    author_email="james.samuel.gainer@gmail.com",
+    description="Fast AutoML for Structured Data: an Insight Data Science Project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JamieGainer/autarchy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['tpot>=0.9.5']
+)
