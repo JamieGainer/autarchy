d6afa249d64c7fbbaa112b72ca32712e10b69bb5
JamieGainer <jgainer137@gmail.com>
2018-10-07 14:45:22 -0700

    Added command line logic

--- autarchy/train_model.py
+++ autarchy/train_model.py
@@ -7,6 +7,8 @@ import sys
 import tensorflow as tf
 from tensorflow import keras
 
+run_params = ['target_column', 'seed']
+
 try:
     input_file_name = sys.argv[1]
 except:
@@ -21,5 +23,22 @@ except:
 
 data_shape = file_data.shape
 
-print(data_shape)
+param_dict = {
+             'target_column': -1,
+             'seed': 42
+             }
+
+for run_param in run_params:
+    argument = '_' + run_param
+    if argument in sys.argv:
+        try:
+            index = sys.argv.index(argument)
+            value = int(sys.argv[index + 1])
+            param_dict[run_param] = value
+        except:
+            print(
+                 'Value of', argument, 'must be integer provided',
+                 'after', argument, '\b.')
+            quit()
+
 
