e879e94404bea0cac279acfe4e6d034e9b15967b
JamieGainer <jgainer137@gmail.com>
2018-10-17 17:43:39 -0700

    Deleted extraneous code

--- autarchy/autarchy.py
+++ autarchy/autarchy.py
@@ -117,29 +117,6 @@ run_param = {
 # Initialize regressor that runs once
 tpot = TPOTRegressor(**run_param)
 
-# divide into X_train, X_test, y_train, y_test
-
-X, y = data[:, :-1], data[:, -1:] 
-y = np.ravel(y)
-
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y,
-	train_size=run_param['train_size'], test_size=run_param['test_size'],
-	random_state = run_param['random_state_for_split'])
-
-# load benchmark data
-
-benchmark_dict = {}
-
-# pick set of benchmarks based on number of features and number of samples
-
-choose_benchmark_key = lambda x, y: None
-
-benchmark_key = choose_benchmark_key(benchmark_dict.keys(), data.shape)
-
-try:
-	benchmarks = benchmark_dict[benchmark_key]
-except KeyError:
-	benchmarks = set([])
 
 
 # Start tpot.  We will replace some of the initial hyperparmeter points with benchmark points
