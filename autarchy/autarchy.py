77a649584b7e5189fea9c4b19932c076bea56164
JamieGainer <jgainer137@gmail.com>
2018-10-17 21:34:56 -0700

    Fixed typo

--- autarchy/autarchy.py
+++ autarchy/autarchy.py
@@ -99,7 +99,7 @@ x_train, x_test,
 y_train, y_test
 ) = train_test_split(
                     data,  target, train_size=train_size,
-                    test_size, random_state=seed_value
+                    test_size=test_size, random_state=seed_value
                     )
 
 val_size = 0.1/train_size
